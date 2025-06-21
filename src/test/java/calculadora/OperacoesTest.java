bcdb8a06d6377033985b1582a12cdf7eb06d60ca
Gabriel Vieira <gabrielvieiragoncalvesgonzaga@gmail.com>
2025-06-21 11:18:58 -0300

    Inserção dos testes simples no método 'TemOperacao'.

--- src/test/java/calculadora/OperacoesTest.java
+++ src/test/java/calculadora/OperacoesTest.java
@@ -16,4 +16,53 @@ public class OperacoesTest {
 
         assertTrue(resultado);
     }
+
+    @Test //Testa se a função reconhece a operação de subtração.
+    public void testTemOperacaoSubtracao(){
+
+        char[] dados = new char[]{'1', '2', '-', '2', '4'};
+
+        Operacoes operacao = new Operacoes();
+
+        boolean resultado = operacao.temOperacao(dados,1, 5, false);
+
+        assertTrue(resultado);
+    }
+
+    @Test //Testa se a função reconhece a operação de multiplicação.
+    public void testTemOperacaoMultiplicacao(){
+
+        char[] dados = new char[]{'1', '2', '*', '2', '4'};
+
+        Operacoes operacao = new Operacoes();
+
+        boolean resultado = operacao.temOperacao(dados,1, 5, false);
+
+        assertTrue(resultado);
+    }
+
+    @Test //Testa se a função reconhece a operação de divisão.
+    public void testTemOperacaoDivisao(){
+
+        char[] dados = new char[]{'1', '2', '/', '2', '4'};
+
+        Operacoes operacao = new Operacoes();
+
+        boolean resultado = operacao.temOperacao(dados,1, 5, false);
+
+        assertTrue(resultado);
+    }
+
+    @Test //Testa se não ativa com número negativo somente.
+    public void testTemOperacao(){
+
+        char[] dados = new char[]{'-', '1', '2', '3', '4'};
+
+        Operacoes operacao = new Operacoes();
+
+        boolean resultado = operacao.temOperacao(dados,1, 5, false);
+
+        assertFalse(resultado);
+    }
+
 }
